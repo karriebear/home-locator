015a9ff92542c116b8358200ebc96f795cd1a7f4
karrie <karrie@streamlit.io>
2020-05-19 19:10:49 -0400

    Process + interact with data

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ metrics = [
     'Natural Disasters',
     'Weather',
     'Income Tax',
-    'Capital Gains Tax',
+    # 'Capital Gains Tax',
     'Sin Tax',
     'Wireless Tax',
     'Sales Tax',
@@ -16,39 +16,86 @@ metrics = [
 ]
 
 metrics_data = {}
+
 ranking_df = False
 
-@st.cache
+def strip_number(n):
+    return float(n.strip('$%'))
+
+@st.cache(allow_output_mutation=True)
 def load_data(file, sheet):
-    global ranking_df
     data = pd.read_csv(
         'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(
             file,
-            urllib.parse.quote(sheet)
-        )
+            urllib.parse.quote(sheet),
+        ),
+        thousands=',',
+        converters={
+            'Tax (%)':strip_number,
+            'Tax ($)':strip_number,
+        }
     )
-    if ranking_df is False:
-        ranking_df = data['State']
-    # metrics_data[sheet][DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    data = data.fillna(0)
+    columns = data.columns[1:]
+    metric_data = {
+        'data': data,
+        'columns': columns,
+        'sub_weights': {},
+        'weight': 0
+    }
+    return metric_data
 
-def create_section_weights(columns):
+def create_section_weights(metric, columns):
+    show_category_weighting = st.sidebar.checkbox('Show category weighting', key=metric)
+    default_value = 1/len(columns)
+
     for col in columns:
-        st.sidebar.text_input(col)
-
-def create_rankings():
-    if ranking_df is not False:
-        st.write(ranking_df)
-    # metrics_data[metric][]/metrics_data[metric][]
+        if show_category_weighting:
+            metrics_data[metric]['sub_weights'][col] = st.sidebar.slider(
+                col,
+                value=metrics_data[metric]['sub_weights'][col] if col in metrics_data[metric]['sub_weights'] else default_value,
+                key='{0}_{1}'.format(metric,col)
+            )
+        elif col in metrics_data[metric]['sub_weights']:
+            continue
+        else:
+            metrics_data[metric]['sub_weights'][col] = default_value
 
 for metric in metrics:
-    data = load_data('1ZBhUjnMxAQW1fgQAEipzJ7jbaJ5768bGSkqyuOgHn2o', metric)
-    metrics_data[metric] = data
-    st.sidebar.subheader(metric)
-    columns = data.columns
-    if len(columns) > 2:
-        create_section_weights(columns[1:])
+    metric_data = load_data('1ZBhUjnMxAQW1fgQAEipzJ7jbaJ5768bGSkqyuOgHn2o', metric)
+    metrics_data[metric] = metric_data
+    columns = metric_data['columns']
 
+    st.sidebar.subheader(metric)
+    weight = st.sidebar.slider('Weight', value=1/len(metrics), key=metric)
+
+    metric_score = metric_data['data'][['State']].copy()
+
+    if len(columns) > 1:
+        create_section_weights(metric, columns)
+
+    # cleanup/move to method -> calculate scores
+    if len(metric_data['sub_weights'].keys()) > 0:
+        metric_score[metric] = 0
+        for col in metrics_data[metric]['sub_weights']:
+            metric_score[metric] = metric_score[metric] + metric_data['data'][col] * metric_data['sub_weights'][col]
+    else:
+        metric_score[metric] = metric_data['data'].iloc[:,1]
+
+    if ranking_df is False:
+        ranking_df = metric_score
+        ranking_df.insert(1, 'Score', 0.0)
+    else:
+        ranking_df = pd.merge(ranking_df, metric_score, on="State")
+
+    ranking_df['Score'] = ranking_df['Score'] + weight * ranking_df[metric]
+    # end cleanup
+
+st.title('Rankings')
+
+ranking_df = ranking_df.sort_values(by=['Score'], ascending=False)
+st.write(ranking_df)
+
+for metric in metrics:
     st.subheader(metric)
-    st.write(metrics_data[metric])
-    create_rankings()
+    st.write(metrics_data[metric].get('data'))
